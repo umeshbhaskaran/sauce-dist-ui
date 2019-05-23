2d31d9bb2a50d3d1894deaa1c4ae199812c82ca2
Umesh Athigari Baskaran <uab@localhost.localdomain>
2019-05-23 10:45:13 -0400

    Updates

---
+++ precache-manifest.708193d224c445f29d662e92d57e5818.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2581c4938c7c21c70d0730f53d0f400",
+    "url": "/sauce-dist-ui/index.html"
+  },
+  {
+    "revision": "196250d172fa3f7c7c4f",
+    "url": "/sauce-dist-ui/static/css/2.f099710a.chunk.css"
+  },
+  {
+    "revision": "8a2ec4e095a7224fb42f",
+    "url": "/sauce-dist-ui/static/css/main.58234e11.chunk.css"
+  },
+  {
+    "revision": "196250d172fa3f7c7c4f",
+    "url": "/sauce-dist-ui/static/js/2.abe7c4e6.chunk.js"
+  },
+  {
+    "revision": "8a2ec4e095a7224fb42f",
+    "url": "/sauce-dist-ui/static/js/main.21955aff.chunk.js"
+  },
+  {
+    "revision": "256d3c95f3de70c07a83",
+    "url": "/sauce-dist-ui/static/js/runtime~main.e3bfd46b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/sauce-dist-ui/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/sauce-dist-ui/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/sauce-dist-ui/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/sauce-dist-ui/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/sauce-dist-ui/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1bc9c15a434fe3ea4ac5769974dcdc0f",
+    "url": "/sauce-dist-ui/static/media/sauce.1bc9c15a.png"
+  }
+]);
